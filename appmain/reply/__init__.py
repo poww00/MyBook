b173c07662c26ee0dbcdf3dc1f437e00633b926d
Jacob <taekdonglee@gmail.com>
2025-02-17 16:32:42 +0900

    second commit

--- appmain/reply/__init__.py
+++ appmain/reply/__init__.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('myBook.db')
 cursor = conn.cursor()
 
 # SQL = 'DROP TABLE replies'
-#
+###
 # cursor.execute(SQL)
 
 SQL = 'CREATE TABLE IF NOT EXISTS replies (replyNo INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT NOT NULL, description TEXT NOT NULL, targetArticle NOT NULL)'
