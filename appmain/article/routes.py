1404023a762aa5a9ec347e0405582333e175c25c
esther <iamesther1315@gmail.com>
2025-02-18 11:24:58 +0900

    routes.py 55p

--- appmain/article/routes.py
+++ appmain/article/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, send_from_directory, make_response, jsonify, request
+from flask import Blueprint, send_from_directory, make_response, jsonify, request, url_for
 import sqlite3
 import os
 from appmain import app
@@ -64,6 +64,89 @@ def createArticle():
     else:
         pass
     return make_response(jsonify(payload), 200) #38p
+
+@article.route('/api/article/recent', methods=['GET'])
+def getRecentArticles():
+    payload = {"success":False}
+
+    conn = sqlite3.connect('myBook.db')
+    cursor = conn.cursor()
+
+    if cursor:
+        SQL = 'SELECT articleNo, author, tilte, category, description, price, picture FROM articles ORDER BY articleNO DESC LIMIT 6'
+        cursor.execute(SQL)
+        result = cursor.fetchall()
+        cursor.close()
+    conn.close()
+
+    recentArticleDics = []
+
+    if len(result) > 0:
+        for article in result:
+            recentArticleDics.append({"articleNo": article[0], "title": article[2], "desc":article[4]})
+            payload = {"success": True, "articles": recentArticleDics}
+            return make_response(jsonify(payload), 200)
+        
+@article.route('/display_article/<int:articleNo>',methods=['GET'])
+def displayArticlePage(articleNo):
+    return send_from_directory(app.root_path,'templates/display_article.html')
+
+def translateCategory(catId):
+    category = '미분류'
+
+    if catId == 0:
+        category = '인문'
+    elif catId == 1:
+        category = '사회과학'
+    elif catId == 1:
+        category = '자연과학'
+    elif catId == 1:
+        category = '의학'
+    elif catId == 1:
+        category = '경제/경영'
+    elif catId == 1:
+        category = '공학'
+    elif catId == 1:
+        category = '음악'
+    elif catId == 1:
+        category = '미술'
+    elif catId == 1:
+        category = '기타'
+    else:
+        category = '미분류'
+
+    return category
+
+@article.route('/api/article/display', methods=['GET','POST'])
+def displayArticle():
+    data=request.form
+    articleNo = data.get("articleNo")
+
+    payload = {"success":False}
+
+    conn = sqlite3.connect('mybook.db')
+    cursor = conn.cursor()
+
+    if cursor:
+        SQL = 'SELECT author, tilte, category, description, price, picture FROM articles WHERE articleNo=?'
+        cursor.execute(SQL,(articleNo))
+        result = cursor.fetchone()
+        cursor.close()
+    conn.close()
+
+    if result:
+        if result[5]:
+            picFilePath = 'pics/' + result[0] + '/' + result[5]
+            picURL = url_for('static',filename=picFilePath,_external=True)
+        else:
+            picURL = None
+
+        article = {"author":result[0],"title":result[1],"category":translateCategory(result[2]),"description":result[3],"price":result[4],"picture":picURL}
+        payload = {"success":True, "article":article}
+    else:
+        payload={"success":True, "article":None}
+
+    return make_response(jsonify(payload),200)
 
 # 상품 게시물 정보 수정 페이지 #69p
 @article.route('/update_article/<int:articleNo>',methods=['GET'])
